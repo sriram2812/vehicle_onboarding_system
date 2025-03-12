6df4a686fa39d7af1b362d683dfe80bcaa9cc677
sriram2812 <127116525+sriram2812@users.noreply.github.com>
2025-03-12 13:05:13 +0530

    Add VehicleService interface

---
+++ src/main/java/com/vehicle/onboard/service/VehicleService.java
@@ -0,0 +1,14 @@
+package com.vehicle.onboard.service;
+
+import com.vehicle.onboard.dto.VehicleRequestDto;
+import com.vehicle.onboard.dto.VehicleResponseDto;
+
+import java.util.List;
+
+public interface VehicleService {
+    VehicleResponseDto createVehicle(VehicleRequestDto requestDto);
+    VehicleResponseDto getVehicleById(Long id);
+    List<VehicleResponseDto> getAllActiveVehicles();
+    VehicleResponseDto updateVehicle(Long id, VehicleRequestDto requestDto);
+    void deactivateVehicle(Long id);
+}
